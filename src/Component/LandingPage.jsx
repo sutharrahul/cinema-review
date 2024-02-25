08c384e4ed694e617962e8915cc07ae213b403e6
Rahul suthar <sutharrahul119@gmail.com>
2024-02-25 22:23:21 +0530

    new styling added :tada

--- src/Component/LandingPage.jsx
+++ src/Component/LandingPage.jsx
@@ -2,12 +2,6 @@ import React from "react";
 import backgroundVideo from "../assets/bgVideo.webm";
 function LandingPage({ children }) {
   return (
-    // <section class="showcase">
-    //   <div class="video-container">
-    //     <video src={backgroundVideo} autoPlay muted loop />
-    //   </div>
-    //   <div class="content">{children}</div>
-    // </section>
     <section className="showcase h-screen flex justify-center text-center text-white bg-black relative">
       <div class="video-container absolute inset-0 overflow-hidden">
         <video
