16a136bb6aa80060ef5ce7671abb49e563368a06
Rahul suthar <sutharrahul119@gmail.com>
2024-03-04 16:28:24 +0530

    search button added

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -11,12 +11,29 @@ function Searchbar() {
           </div>
         </div>
 
-        <div className="form-control w-full ">
+        <div className="form-control w-full flex-row justify-between">
           <input
             type="text"
             placeholder="Search"
             className="input input-bordered input-accent w-full rounded-full text-[#ffb86c]"
           />
+          <button className="btn btn-accent rounded-full ml-3">
+            <svg
+              className="w-4 h-4"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+          </button>
         </div>
       </div>
     </div>
